181373ce01f0fa43285b52e463eb2e5f2a3407d8
Hiroaki Yutani <yutani.ini@gmail.com>
2021-11-03 08:59:39 +0900

    Update generated bindings (#83)
    Update generated bindings (#84)

--- bindings/bindings-linux-x86_64-R4.2-devel.rs
+++ bindings/bindings-linux-x86_64-R4.2-devel.rs
@@ -240,6 +240,15 @@ pub const NAMEDMAX: u32 = 7;
 pub const R_XDR_DOUBLE_SIZE: u32 = 8;
 pub const R_XDR_INTEGER_SIZE: u32 = 4;
 pub const R_CODESET_MAX: u32 = 63;
+pub const IDENT_NUM_AS_BITS: u32 = 1;
+pub const IDENT_NA_AS_BITS: u32 = 2;
+pub const IDENT_ATTR_BY_ORDER: u32 = 4;
+pub const IDENT_USE_BYTECODE: u32 = 8;
+pub const IDENT_USE_CLOENV: u32 = 16;
+pub const IDENT_USE_SRCREF: u32 = 32;
+pub const IDENT_EXTPTR_AS_REF: u32 = 64;
+pub const HT_TYPE_IDENTICAL: u32 = 0;
+pub const HT_TYPE_ADDRESS: u32 = 1;
 pub const _STDINT_H: u32 = 1;
 pub const _BITS_WCHAR_H: u32 = 1;
 pub const _BITS_STDINT_INTN_H: u32 = 1;
@@ -287,12 +296,13 @@ pub const R_MAJOR: &'static [u8; 2usize] = b"4\0";
 pub const R_MINOR: &'static [u8; 4usize] = b"2.0\0";
 pub const R_STATUS: &'static [u8; 29usize] = b"Under development (unstable)\0";
 pub const R_YEAR: &'static [u8; 5usize] = b"2021\0";
-pub const R_MONTH: &'static [u8; 3usize] = b"08\0";
-pub const R_DAY: &'static [u8; 3usize] = b"31\0";
-pub const R_SVN_REVISION: u32 = 80839;
+pub const R_MONTH: &'static [u8; 3usize] = b"12\0";
+pub const R_DAY: &'static [u8; 3usize] = b"17\0";
+pub const R_SVN_REVISION: u32 = 81389;
 pub const R_GE_definitions: u32 = 13;
 pub const R_GE_deviceClip: u32 = 14;
-pub const R_GE_version: u32 = 14;
+pub const R_GE_group: u32 = 15;
+pub const R_GE_version: u32 = 15;
 pub const MAX_GRAPHICS_SYSTEMS: u32 = 24;
 pub const R_USE_PROTOTYPES: u32 = 1;
 pub const leftButton: u32 = 1;
@@ -313,6 +323,47 @@ pub const R_GE_patternExtendPad: u32 = 1;
 pub const R_GE_patternExtendRepeat: u32 = 2;
 pub const R_GE_patternExtendReflect: u32 = 3;
 pub const R_GE_patternExtendNone: u32 = 4;
+pub const R_GE_compositeClear: u32 = 1;
+pub const R_GE_compositeSource: u32 = 2;
+pub const R_GE_compositeOver: u32 = 3;
+pub const R_GE_compositeIn: u32 = 4;
+pub const R_GE_compositeOut: u32 = 5;
+pub const R_GE_compositeAtop: u32 = 6;
+pub const R_GE_compositeDest: u32 = 7;
+pub const R_GE_compositeDestOver: u32 = 8;
+pub const R_GE_compositeDestIn: u32 = 9;
+pub const R_GE_compositeDestOut: u32 = 10;
+pub const R_GE_compositeDestAtop: u32 = 11;
+pub const R_GE_compositeXor: u32 = 12;
+pub const R_GE_compositeAdd: u32 = 13;
+pub const R_GE_compositeSaturate: u32 = 14;
+pub const R_GE_compositeMultiply: u32 = 15;
+pub const R_GE_compositeScreen: u32 = 16;
+pub const R_GE_compositeOverlay: u32 = 17;
+pub const R_GE_compositeDarken: u32 = 18;
+pub const R_GE_compositeLighten: u32 = 19;
+pub const R_GE_compositeColorDodge: u32 = 20;
+pub const R_GE_compositeColorBurn: u32 = 21;
+pub const R_GE_compositeHardLight: u32 = 22;
+pub const R_GE_compositeSoftLight: u32 = 23;
+pub const R_GE_compositeDifference: u32 = 24;
+pub const R_GE_compositeExclusion: u32 = 25;
+pub const R_GE_nonZeroWindingRule: u32 = 1;
+pub const R_GE_evenOddRule: u32 = 2;
+pub const R_GE_alphaMask: u32 = 1;
+pub const R_GE_luminanceMask: u32 = 2;
+pub const R_GE_capability_semiTransparency: u32 = 0;
+pub const R_GE_capability_transparentBackground: u32 = 1;
+pub const R_GE_capability_rasterImage: u32 = 2;
+pub const R_GE_capability_capture: u32 = 3;
+pub const R_GE_capability_locator: u32 = 4;
+pub const R_GE_capability_events: u32 = 5;
+pub const R_GE_capability_patterns: u32 = 6;
+pub const R_GE_capability_clippingPaths: u32 = 7;
+pub const R_GE_capability_masks: u32 = 8;
+pub const R_GE_capability_compositing: u32 = 9;
+pub const R_GE_capability_transformations: u32 = 10;
+pub const R_GE_capability_paths: u32 = 11;
 pub type size_t = ::std::os::raw::c_ulong;
 pub type wchar_t = ::std::os::raw::c_int;
 #[repr(C)]
@@ -3834,6 +3885,9 @@ extern "C" {
     pub fn Rf_findVarInFrame3(arg1: SEXP, arg2: SEXP, arg3: Rboolean) -> SEXP;
 }
 extern "C" {
+    pub fn R_existsVarInFrame(arg1: SEXP, arg2: SEXP) -> Rboolean;
+}
+extern "C" {
     pub fn R_removeVarFromFrame(arg1: SEXP, arg2: SEXP);
 }
 extern "C" {
@@ -4942,6 +4996,77 @@ pub const SORTED_INCR: _bindgen_ty_2 = 1;
 pub const SORTED_INCR_NA_1ST: _bindgen_ty_2 = 2;
 pub const KNOWN_UNSORTED: _bindgen_ty_2 = 0;
 pub type _bindgen_ty_2 = i32;
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct R_hashtab_type {
+    pub cell: SEXP,
+}
+#[test]
+fn bindgen_test_layout_R_hashtab_type() {
+    assert_eq!(
+        ::std::mem::size_of::<R_hashtab_type>(),
+        8usize,
+        concat!("Size of: ", stringify!(R_hashtab_type))
+    );
+    assert_eq!(
+        ::std::mem::align_of::<R_hashtab_type>(),
+        8usize,
+        concat!("Alignment of ", stringify!(R_hashtab_type))
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<R_hashtab_type>())).cell as *const _ as usize },
+        0usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(R_hashtab_type),
+            "::",
+            stringify!(cell)
+        )
+    );
+}
+extern "C" {
+    pub fn R_asHashtable(h: SEXP) -> R_hashtab_type;
+}
+extern "C" {
+    pub fn R_HashtabSEXP(h: R_hashtab_type) -> SEXP;
+}
+extern "C" {
+    pub fn R_isHashtable(h: SEXP) -> ::std::os::raw::c_int;
+}
+extern "C" {
+    pub fn R_mkhashtab(type_: ::std::os::raw::c_int, arg1: ::std::os::raw::c_int)
+        -> R_hashtab_type;
+}
+extern "C" {
+    pub fn R_gethash(h: R_hashtab_type, key: SEXP, nomatch: SEXP) -> SEXP;
+}
+extern "C" {
+    pub fn R_sethash(h: R_hashtab_type, key: SEXP, value: SEXP) -> SEXP;
+}
+extern "C" {
+    pub fn R_remhash(h: R_hashtab_type, key: SEXP) -> ::std::os::raw::c_int;
+}
+extern "C" {
+    pub fn R_numhash(h: R_hashtab_type) -> ::std::os::raw::c_int;
+}
+extern "C" {
+    pub fn R_typhash(h: R_hashtab_type) -> ::std::os::raw::c_int;
+}
+extern "C" {
+    pub fn R_maphash(h: R_hashtab_type, FUN: SEXP) -> SEXP;
+}
+extern "C" {
+    pub fn R_maphashC(
+        h: R_hashtab_type,
+        FUN: ::std::option::Option<
+            unsafe extern "C" fn(arg1: SEXP, arg2: SEXP, arg3: *mut ::std::os::raw::c_void),
+        >,
+        data: *mut ::std::os::raw::c_void,
+    );
+}
+extern "C" {
+    pub fn R_clrhash(h: R_hashtab_type);
+}
 extern "C" {
     pub fn SET_TYPEOF(x: SEXP, v: ::std::os::raw::c_int);
 }
@@ -6265,13 +6390,33 @@ pub struct _DevDesc {
     pub releaseMask: ::std::option::Option<unsafe extern "C" fn(ref_: SEXP, dd: pDevDesc)>,
     pub deviceVersion: ::std::os::raw::c_int,
     pub deviceClip: Rboolean,
+    pub defineGroup: ::std::option::Option<
+        unsafe extern "C" fn(
+            source: SEXP,
+            op: ::std::os::raw::c_int,
+            destination: SEXP,
+            dd: pDevDesc,
+        ) -> SEXP,
+    >,
+    pub useGroup:
+        ::std::option::Option<unsafe extern "C" fn(ref_: SEXP, trans: SEXP, dd: pDevDesc)>,
+    pub releaseGroup: ::std::option::Option<unsafe extern "C" fn(ref_: SEXP, dd: pDevDesc)>,
+    pub stroke:
+        ::std::option::Option<unsafe extern "C" fn(path: SEXP, gc: pGEcontext, dd: pDevDesc)>,
+    pub fill: ::std::option::Option<
+        unsafe extern "C" fn(path: SEXP, rule: ::std::os::raw::c_int, gc: pGEcontext, dd: pDevDesc),
+    >,
+    pub fillStroke: ::std::option::Option<
+        unsafe extern "C" fn(path: SEXP, rule: ::std::os::raw::c_int, gc: pGEcontext, dd: pDevDesc),
+    >,
+    pub capabilities: ::std::option::Option<unsafe extern "C" fn(cap: SEXP) -> SEXP>,
     pub reserved: [::std::os::raw::c_char; 64usize],
 }
 #[test]
 fn bindgen_test_layout__DevDesc() {
     assert_eq!(
         ::std::mem::size_of::<_DevDesc>(),
-        592usize,
+        648usize,
         concat!("Size of: ", stringify!(_DevDesc))
     );
     assert_eq!(
@@ -7022,8 +7167,78 @@ fn bindgen_test_layout__DevDesc() {
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<_DevDesc>())).reserved as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).defineGroup as *const _ as usize },
         528usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(defineGroup)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).useGroup as *const _ as usize },
+        536usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(useGroup)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).releaseGroup as *const _ as usize },
+        544usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(releaseGroup)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).stroke as *const _ as usize },
+        552usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(stroke)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).fill as *const _ as usize },
+        560usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(fill)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).fillStroke as *const _ as usize },
+        568usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(fillStroke)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).capabilities as *const _ as usize },
+        576usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_DevDesc),
+            "::",
+            stringify!(capabilities)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_DevDesc>())).reserved as *const _ as usize },
+        584usize,
         concat!(
             "Offset of field: ",
             stringify!(_DevDesc),
@@ -7201,6 +7416,7 @@ pub struct _GEDevDesc {
     pub recordGraphics: Rboolean,
     pub gesd: [*mut GESystemDesc; 24usize],
     pub ask: Rboolean,
+    pub appending: Rboolean,
 }
 #[test]
 fn bindgen_test_layout__GEDevDesc() {
@@ -7302,6 +7518,16 @@ fn bindgen_test_layout__GEDevDesc() {
             stringify!(_GEDevDesc),
             "::",
             stringify!(ask)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_GEDevDesc>())).appending as *const _ as usize },
+        244usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_GEDevDesc),
+            "::",
+            stringify!(appending)
         )
     );
 }
@@ -7816,6 +8042,21 @@ extern "C" {
 }
 extern "C" {
     pub fn R_GE_tilingPatternExtend(pattern: SEXP) -> ::std::os::raw::c_int;
+}
+extern "C" {
+    pub fn R_GE_clipPathFillRule(path: SEXP) -> ::std::os::raw::c_int;
+}
+extern "C" {
+    pub fn GEStroke(path: SEXP, gc: pGEcontext, dd: pGEDevDesc);
+}
+extern "C" {
+    pub fn GEFill(path: SEXP, rule: ::std::os::raw::c_int, gc: pGEcontext, dd: pGEDevDesc);
+}
+extern "C" {
+    pub fn GEFillStroke(path: SEXP, rule: ::std::os::raw::c_int, gc: pGEcontext, dd: pGEDevDesc);
+}
+extern "C" {
+    pub fn R_GE_maskType(mask: SEXP) -> ::std::os::raw::c_int;
 }
 pub type __builtin_va_list = [__va_list_tag; 1usize];
 #[repr(C)]
