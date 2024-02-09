50cfdae211bf201a810df851d5ee0f932d7bd306
Mossa <cgmossa@gmail.com>
2024-02-10 00:52:05 +0100

    fix: `TYPEOF` is no longer cast needlessly

--- src/lib.rs
+++ src/lib.rs
@@ -179,7 +179,7 @@ mod tests {
         unsafe {
             let val = Rf_protect(R_ParseEvalString(cstr!("1"), R_NilValue));
             Rf_PrintValue(val);
-            assert_eq!(TYPEOF(val) as u32, REALSXP);
+            assert_eq!(TYPEOF(val), REALSXP);
             assert_eq!(*REAL(val), 1.);
             Rf_unprotect(1);
         }
